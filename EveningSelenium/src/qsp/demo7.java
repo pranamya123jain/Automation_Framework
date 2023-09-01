760c05876bd7ccdf5c31f80fabbc753f0e39cbfe
LENOVO <LENOVO@DESKTOP-BL08O2D>
2023-09-01 17:28:19 +0530

    Build3

--- EveningSelenium/src/qsp/demo7.java
+++ EveningSelenium/src/qsp/demo7.java
@@ -7,9 +7,9 @@ public class demo7 {
 
 	public static void main(String[] args) 
 	{
-		System.setProperty("webdriver.chromedriver","./Softwars./chromedriver.exe");
-		WebDriver deriver= new ChromeDriver();
-		driver.get("file:///C:/Users/LENOVO/Desktop/link.html");
+		//System.setProperty("webdriver.chromedriver","./Softwars./chromedriver.exe");
+		//WebDriver deriver= new ChromeDriver();
+		//driver.get("file:///C:/Users/LENOVO/Desktop/link.html");
 		
 		
 		
